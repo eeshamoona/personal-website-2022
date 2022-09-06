44dd65dd91940ce732034235a0c4f1320a111c47
eeshamoona <eeshamoona@gmail.com>
2022-09-06 16:57:46 -0500

    Created pages for website

--- personal-website/src/App.tsx
+++ personal-website/src/App.tsx
@@ -1,9 +1,55 @@
-import React from "react";
+import React, { useRef } from "react";
+import "./index.css";
+import "./styles/navbarStyles.css";
 
 function App() {
+  const homeSection = useRef<null | HTMLDivElement>(null);
+  const workSection = useRef<null | HTMLDivElement>(null);
+  const projectSection = useRef<null | HTMLDivElement>(null);
+  const contactSection = useRef<null | HTMLDivElement>(null);
+
+  const scrollDown = (ref: React.RefObject<HTMLDivElement>) => {
+    if (ref) {
+      window.scrollTo({
+        top: ref.current?.offsetTop,
+        behavior: "smooth",
+      });
+    }
+  };
+
   return (
     <div className="App">
-      <p> Hello World!</p>
+      <div className="navbar">
+        <img className="logo" alt="logo" src="eeshamoonalogo.png"></img>
+        <ul className="nav-links">
+          <li className="link" onClick={() => scrollDown(homeSection)}>
+            Home
+          </li>
+          <li className="link" onClick={() => scrollDown(workSection)}>
+            Work Experience
+          </li>
+          <li className="link" onClick={() => scrollDown(projectSection)}>
+            Projects
+          </li>
+          <li className="link" onClick={() => scrollDown(contactSection)}>
+            Contact
+          </li>
+        </ul>
+        <button className="resume"> Download Resume</button>
+      </div>
+
+      <div className="section section2" ref={homeSection}>
+        <h2>Home</h2>
+      </div>
+      <div className="section section3" ref={workSection}>
+        <h2>Work Experience</h2>
+      </div>
+      <div className="section section3" ref={projectSection}>
+        <h2>Projects</h2>
+      </div>
+      <div className="section section4" ref={contactSection}>
+        <h2>Contact Me</h2>
+      </div>
     </div>
   );
 }
---
+++ personal-website/src/components/NavBar.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+
+const NavBar = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default NavBar
