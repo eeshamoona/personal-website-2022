9749c976642b9233deba34ba84cfa211fcbcff2b
eeshamoona <eeshamoona@gmail.com>
2022-09-08 12:24:21 -0500

    Created ContactView

--- personal-website/src/views/ContactView.tsx
+++ personal-website/src/views/ContactView.tsx
@@ -1,17 +1,66 @@
 import React from "react";
 import "../styles/homeviewStyles.css";
+import "../styles/contactviewStyles.css";
+
+import { FaLinkedin, FaInstagram, FaGithubSquare } from "react-icons/fa";
+import { MdMailOutline } from "react-icons/md";
 
 function ContactView() {
   return (
-    <div className="home-container">
-      <p> Hi My name is Eesha!</p>
-      <p> This is the contact page</p>
+    <div className="contact-container">
+      <div className="connect-containter ">
+        <div className="name-container">
+          {"CONNECT WITH ME".split("").map((element, index) => {
+            return (
+              <div className="name-div">
+                <div className="top-div"></div>
+                <div className="bottom-div"></div>
+                <div className="name-h1">
+                  <h2 className="main-header">{element}</h2>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      <p className="subheader">
+        I'm currently looking for full-time opportunities in full-stack web
+        development and software development.
+      </p>
 
-      <div>
-        <button> LinkedIn</button>
-        <button> Email</button>
-        <button> Instagram</button>
-        <button> Github</button>
+      <div className="logos">
+        <a
+          href="https://www.linkedin.com/in/eeshamoona/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="link-logo"
+        >
+          <FaLinkedin />
+        </a>
+        <a
+          href="mailto:eeshamoona@gmail.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="link-logo"
+        >
+          <MdMailOutline />
+        </a>
+        <a
+          href="https://www.instagram.com/eeshamoona/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="link-logo"
+        >
+          <FaInstagram />
+        </a>
+        <a
+          href="https://github.com/eeshamoona"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="link-logo"
+        >
+          <FaGithubSquare />
+        </a>
       </div>
     </div>
   );
