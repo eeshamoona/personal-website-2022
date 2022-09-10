e8b5f2e29aa7d6d09ef7b84ff2640ec51d6cabe7
eeshamoona <eeshamoona@gmail.com>
2022-09-10 18:01:57 -0500

    Creating Work Experience page

--- personal-website/src/views/WorkExperienceView.tsx
+++ personal-website/src/views/WorkExperienceView.tsx
@@ -1,24 +1,37 @@
 import React from "react";
 import "../styles/homeviewStyles.css";
+import "../styles/workexperienceviewStyles.css";
 
 function WorkExperienceView() {
   return (
-    <div className="home-container">
-      <p> Hi My name is Eesha!</p>
-      <p> This is the work experience section</p>
-
-      <div>
-        <div>
-          <p>Meta</p>
+    <div className="work">
+      <div className="workExperience-containter">
+        <div className="name-container">
+          {"WORK EXPERIENCE".split("").map((element, index) => {
+            return (
+              <div className="name-div">
+                <div className="top-div"></div>
+                <div className="bottom-div"></div>
+                <div className="name-h1">
+                  <h2 className="main-header">{element}</h2>
+                </div>
+              </div>
+            );
+          })}
         </div>
-        <div>
-          <p> Motorola Solutions</p>
+      </div>
+      <div className="work-container">
+        <div className="work-div">
+          <h2>Meta</h2>
         </div>
-        <div>
-          <p> Cargill</p>
+        <div className="work-div">
+          <h2> Motorola Solutions</h2>
         </div>
-        <div>
-          <p> SAP</p>
+        <div className="work-div">
+          <h2> Cargill</h2>
+        </div>
+        <div className="work-div">
+          <h2> SAP</h2>
         </div>
       </div>
     </div>
